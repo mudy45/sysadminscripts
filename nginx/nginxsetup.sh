455a2d17494d723d43a567c6622c6cf9bb1da574
mudy45 <mudy45@gmail.com>
2017-09-17 04:05:50 +0700

    Update nginxsetup.sh

--- nginx/nginxsetup.sh
+++ nginx/nginxsetup.sh
@@ -8,16 +8,18 @@
 ###-----------------------------------------------------------------###
 ###                                                               	###
 ###                                                               	###
-###                                                               	###
-###                                                               	###
+### Fixed folder:                                                 	###
+### Nginx User = www-data											###
+### Nginx HTTP Folder = /var/www                                  	###
+### Nginx Log Folder = /var/log/nginx                             	###
+### Nginx Cache Folder = /var/cache/nginx                          	###
+###                                                                	###
 #######################################################################
 cpuCount=$(nproc --all)
 currentPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ###-----> Dhparam generation bits
 dhparamFile = "/etc/nginx/ssl/dhparam.pem"
 dhparamBits = "4096"
-###-----> Nginx user & group = www-data or nginx
-nginxUser="www-data"
 ###-----> Only review versions, to choose target versions & repo
 doReviewonly = "0"
 ###-----> Disable on re-compile, no more apt-get
@@ -32,9 +34,14 @@ useRepoppasta = "0"
 useReponginx = "0"
 useRepolinux = "0"
 
+###-----> Run compile script here, delete everything for cleanup
+buildDir = "/usr/local/src/buildnginx"
+###-----> $buildDir/sourceset-$sourceset is where all source downloaded
+###-----> change sourceSet number to create new source set
+###-----> backup entire folder to recompile exactly same package
 sourceSet = "1"
-buildDir = "/usr/local/src/buildnginx
 
+###-----> -R Review Only, -C Standard Compile
 if [ "$1" == "--review" -o "$1" == "-r" ]; then
 	doReviewonly = "1"
 	doDhparam = "0"
@@ -45,13 +52,39 @@ if [ "$1" == "--review" -o "$1" == "-r" ]; then
 	useRepoppasta = "1"
 	useReponginx = "1"
 	useRepolinux = "1"
-fi
-
-if [ "$1" == "--compile" -o "$1" == "-c" ]; then
+elif [ "$1" == "--compile" -o "$1" == "-c" ]; then
 	doReviewonly = "0"
 	doCompile = "1"
 	doCleanup = "0"
 fi
+
+#######################################################################
+### Prepare process													###
+###                                                               	###
+###-----------------------------------------------------------------###
+prepareonce()
+{
+#######################################################################
+### Check and Set Nginx User										###
+###                                                               	###
+###-----------------------------------------------------------------###
+local nginxUserExists=$(id -u www-data > /dev/null 2>&1; echo $?)
+if [ "${nginxUserExists}" != "0" ];
+then
+	useradd -d /etc/nginx -s /bin/false nginx
+fi
+#######################################################################
+### Generate DHParam for Nginx OpemSSL								###
+### Will take very long time   						               	###
+###-----------------------------------------------------------------###
+if [ "${doDhparam}" != "0" ];
+then
+	openssl dhparam -out ${dhparamFile} ${dhparamBits}
+fi
+###-----------------------------------------------------------------###
+}
+#######################################################################
+
 
 #######################################################################
 ### Review Target & Repo for Nginx Package						  	###
@@ -60,16 +93,18 @@ fi
 ### http://nginx.org/en/download.html								###
 ### current stable		: nginx 1.12.1 & 1.13.3						###
 ###-----------------------------------------------------------------###
-if [ "${doUbuntu}" != "1" ];
-then
-	linuxVer  = "xenial"
-	repoNginx = "https://nginx.org/packages/ubuntu"
-	repoLinux = "https://sgp1.digitalocean.com/ubuntu"
-else
-	linuxVer  = "stretch-backports"
-	repoNginx = "https://nginx.org/packages/debian/"
-	repoLinux = "https://sgp1.digitalocean.com/debian"
-fi
+getnginxsource()
+{
+	if [ "${doUbuntu}" != "1" ];
+	then
+		linuxVer  = "xenial"
+		repoNginx = "https://nginx.org/packages/ubuntu"
+		repoLinux = "https://sgp1.digitalocean.com/ubuntu"
+	else
+		linuxVer  = "stretch-backports"
+		repoNginx = "https://nginx.org/packages/debian/"
+		repoLinux = "https://sgp1.digitalocean.com/debian"
+	fi
 ###-----------------------------------------------------------------###
 ### Choose Ubuntu or Debian, run Review, fill table below, decide  	###
 ### your distro, target build, repo, check module, then compile.  	###
@@ -98,64 +133,39 @@ fi
 ### APT Repository												 	###
 ###                                                               	###
 ###-----------------------------------------------------------------###
-if [ "${doGetapt}" != "0" ];
-then
-	wget -N https://nginx.org/keys/nginx_signing.key
-	apt-key add nginx_signing.key
-	rm nginx_signing.key
-	if [ "${useRepolinux}" != "0" ];
+	if [ "${doGetapt}" != "0" ];
 	then
-		add-apt-repository -s -y "deb ${repoLinux} ${ubuntuVer} nginx"
-		add-apt-repository -s -y "deb-src ${repoLinux} ${ubuntuVer} nginx"
-	fi
-	if [ "${useReponginx}" != "0" ];
-	then
-		add-apt-repository -s -y "deb ${repoNginx} ${ubuntuVer} nginx"
-		add-apt-repository -s -y "deb-src ${repoNginx} ${ubuntuVer} nginx"
-	fi
-	if [ "${useRepoppasta}" != "0" ];
-	then
-		add-apt-repository -s -y ppa:nginx/stable
-	fi
-	if [ "${useRepoppadev}" != "0" ];
-	then
+		wget -N https://nginx.org/keys/nginx_signing.key
+		apt-key add nginx_signing.key
+		rm nginx_signing.key
+		cd ${buildDir}
+		if [ "${useRepolinux}" != "0" ];
+		then
+			add-apt-repository -s -y "deb ${repoLinux} ${ubuntuVer} nginx"
+			add-apt-repository -s -y "deb-src ${repoLinux} ${ubuntuVer} nginx"
+		fi
+		if [ "${useReponginx}" != "0" ];
+		then
+			add-apt-repository -s -y "deb ${repoNginx} ${ubuntuVer} nginx"
+			add-apt-repository -s -y "deb-src ${repoNginx} ${ubuntuVer} nginx"
+		fi
+		if [ "${useRepoppasta}" != "0" ];
+		then
+			add-apt-repository -s -y ppa:nginx/stable
+		fi
+		if [ "${useRepoppadev}" != "0" ];
+		then
 		add-apt-repository -s -y ppa:nginx/development
+		fi
+		apt-get update
+		apt-get upgrade
+		###-----> Troubleshooting package missing stuff					###
+		#apt-get install software-properties-common phyton-software-properties 
 	fi
-	apt-get update
-	apt-get upgrade
-	###-----> Troubleshooting package missing stuff					###
-	#apt-get install software-properties-common phyton-software-properties 
-fi
 ###-----------------------------------------------------------------###
-#######################################################################
-
-
-prepareonce()
-{
-#######################################################################
-### Prepare process													###
-###                                                               	###
-###-----------------------------------------------------------------###
-#######################################################################
-### Check and Set Nginx User										###
-###                                                               	###
-###-----------------------------------------------------------------###
-local nginxUserExists=$(id -u ${nginxUser} > /dev/null 2>&1; echo $?)
-if [ "${nginxUserExists}" != "0" ];
-then
-	useradd -d /etc/nginx -s /bin/false nginx
-fi
-#######################################################################
-### Generate DHParam for Nginx OpemSSL								###
-### Will take very long time   						               	###
-###-----------------------------------------------------------------###
-if [ "${doDhparam}" != "0" ];
-then
-	openssl dhparam -out ${dhparamFile} ${dhparamBits}
-fi
-###-----------------------------------------------------------------###
-#######################################################################
 }
+#######################################################################
+
 
 
 #######################################################################
@@ -164,41 +174,73 @@ fi
 ###-----------------------------------------------------------------###
 ###                                                               	###
 ###                                                               	###
-###-----> 
-verOpenssl="1.0.2l"
-###-----> 
-verPagespeed="1.12.34.2"
-###-----> 
-pcreVers="8.40"
-###-----> 
-zlibVers="1.2.11"
+	###-----> 
+	verOpenssl="1.0.2l"
+	###-----> 
+	verPagespeed="1.12.34.2"
+	###-----> 
+	pcreVers="8.40"
+	###-----> 
+	zlibVers="1.2.11"
+
+
 
 #######################################################################
-### Download and decompress modules									###
-### Compile Brotli Module                                         	###
+### Get and Prepare Modules										 	###
+###                                                               	###
 ###-----------------------------------------------------------------###
-cd /usr/local/src/github/brotli \
-&& python setup.py install
-cd /usr/local/src/github/libbrotli \
-&& ./autogen.sh \
-&& ./configure \
-&& make -j ${cpuCount} \
-&& make install
-cd /usr/local/src/github/ngx_brotli \
-&& git submodule update --init
+###                                                               	###
+###                                                               	###
+getmodule()
+{
+	cd ${buildDir}
+	###-----> https://github.com/nbs-system/naxsi/releases
+
+	git clone https://github.com/nginx/nginx.git 
+	git clone https://github.com/simpl/ngx_devel_kit.git 
+	git clone https://github.com/openresty/headers-more-nginx-module.git 
+	git clone https://github.com/vozlt/nginx-module-vts.git 
+	git clone https://github.com/google/brotli.git 
+	git clone https://github.com/bagder/libbrotli 	
+	git clone https://github.com/google/ngx_brotli 
+	git clone https://github.com/nbs-system/naxsi.git 
+	git clone https://github.com/openresty/set-misc-nginx-module.git
 
 #######################################################################
 ### Download and decompress modules									###
 ### PCRE, OpenSSL, ZLib                                           	###
 ###-----------------------------------------------------------------###
-cd ${buildDir}/downloads-${sourceSet} \
-&& wget -N https://www.openssl.org/source/openssl-${openSslVers}.tar.gz \
-&& wget -N https://ftp.pcre.org/pub/pcre/pcre-${pcreVers}.tar.gz \
-&& wget -N http://www.zlib.net/zlib-${zlibVers}.tar.gz \
-&& tar xvf openssl-${openSslVers}.tar.gz --strip-components=1 -C ${buildDir}/packages/openssl \
-&& tar xvf pcre-${pcreVers}.tar.gz --strip-components=1 -C ${buildDir}/packages/pcre \
-&& tar xvf zlib-${zlibVers}.tar.gz --strip-components=1 -C ${buildDir}/packages/zlib
+	cd ${buildDir}/sourceset-${sourceSet} \
+	&& wget -N https://www.openssl.org/source/openssl-${openSslVers}.tar.gz \
+	&& wget -N https://ftp.pcre.org/pub/pcre/pcre-${pcreVers}.tar.gz \
+	&& wget -N http://www.zlib.net/zlib-${zlibVers}.tar.gz \
+	&& tar xvf openssl-${openSslVers}.tar.gz --strip-components=1 -C ${buildDir}/packages/openssl \
+	&& tar xvf pcre-${pcreVers}.tar.gz --strip-components=1 -C ${buildDir}/packages/pcre \
+	&& tar xvf zlib-${zlibVers}.tar.gz --strip-components=1 -C ${buildDir}/packages/zlib
+}
 
+
+#######################################################################
+### Download and decompress modules									###
+### Compile Brotli Module                                         	###
+###-----------------------------------------------------------------###
+cd ${buildDir}/brotli \
+&& python setup.py install
+cd ${buildDir}/libbrotli \
+&& ./autogen.sh \
+&& ./configure \
+&& make -j ${cpuCount} \
+&& make install
+cd ${buildDir}/ngx_brotli \
+&& git submodule update --init
+
+
+
+
+#######################################################################
+### Compile Nginx													###
+###                                                               	###
+###-----------------------------------------------------------------###
 nginxCompile()
 {
 cd ${buildDir}/nginx \
@@ -214,8 +256,8 @@ cd ${buildDir}/nginx \
                     --http-fastcgi-temp-path=/var/cache/nginx/fastcgi \
                     --http-uwsgi-temp-path=/var/cache/nginx/uwsgi \
                     --http-scgi-temp-path=/var/cache/nginx/scgi \
-                    --user=${nginxUser} \
-                    --group=${nginxUser} \
+                    --user=www-data \
+                    --group=www-data \
                     --with-poll_module \
                     --with-threads \
                     --with-file-aio \
@@ -258,41 +300,27 @@ cd ${buildDir}/nginx \
     && make install
 }
 
+
+#######################################################################
+### Configure Nginx													###
+###                                                               	###
+###-----------------------------------------------------------------###
 nginxConfigure()
 {
-    #+------------------------------------------------------------------------+
-    #+ Remove *.default files from configuration directory.
-    #+------------------------------------------------------------------------+
-    rm -rf /etc/nginx/config/*.default
+	rm -rf /etc/nginx/config/*.default
+    rm /etc/nginx/nginx.conf
+    rm /etc/nginx/fastcgi.conf
+    rm /etc/nginx/fastcgi_params
 
-    #+------------------------------------------------------------------------+
-    #+ Remove default NGINX configuration file.
-    #+------------------------------------------------------------------------+
-    rm /etc/nginx/config/nginx.conf
-
-    #+------------------------------------------------------------------------+
-    #+ Remove default FastCGI Configuration
-    #+------------------------------------------------------------------------+
-    rm /etc/nginx/config/fastcgi.conf \
-    && rm /etc/nginx/config/fastcgi_params
-
-    #+------------------------------------------------------------------------+
-    #+ Copy new configuration
-    #+------------------------------------------------------------------------+
-    cp -R ${currentPath}/html/index.html /home/nginx/htdocs/public/index.html \
-    && cp -R ${currentPath}/nginx/* /etc/nginx \
-    && cp -R ${currentPath}/systemd/nginx.service /lib/systemd/system/nginx.service
-
-    #+------------------------------------------------------------------------+
-    #+ Set correct permissions and ownership
-    #+------------------------------------------------------------------------+
-    chown -R nginx:nginx /home/nginx
-
-    #+------------------------------------------------------------------------+
-    #+ Create systemd service script and start NGINX
-    #+------------------------------------------------------------------------+
-    systemctl enable nginx \
-    && systemctl start nginx
+	cp -R ${currentPath}/html/index.html /var/www/index.html
+    cp -R ${currentPath}/nginx/* /etc/nginx
+    cp -R ${currentPath}/systemd/nginx.service /lib/systemd/system/nginx.service
+    chown -R www-data:www-data /var/www
+	chown -R www-data:www-data /var/cache/nginx
+	chown -R www-data:www-data /var/log/nginx
+	
+    systemctl enable nginx
+    systemctl start nginx
 }
 
 
