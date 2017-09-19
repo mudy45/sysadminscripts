2aa72688dcb6a944426e33220103bd5356a28545
mudy45 <mudy45@gmail.com>
2017-09-20 00:37:44 +0700

    Update resetrootpassword.sh

--- mysql/resetrootpassword.sh
+++ mysql/resetrootpassword.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env  bash
 
 systemctl stop mysql
+mysqld_safe --skip-grant-tables --skip-networking &
+mysql -u root -D mysql -bse "UPDATE user SET password=PASSWORD("$1") WHERE user='root'; FLUSH PRIVILEGES; EXIT;"
+rm /root/.my.cnf
+echo "#
+[client]
+user=root
+pass=$1" > /root/.my.cnf
+systemctl start mysql
+systemctl status mysql
 
-mysqld_safe --skip-grant-tables &
-
-MariaDB [(none)]> use mysql;
-MariaDB [mysql]> UPDATE user SET password=PASSWORD("new_password") WHERE User='root';
-MariaDB [mysql]> FLUSH PRIVILEGES;
-MariaDB [mysql]> quit;
-
-
-
-
+### EOF
