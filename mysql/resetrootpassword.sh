54e2b4f10a238595d9fffe88dfb6a6b62ed7020c
mudy45 <mudy45@gmail.com>
2017-09-20 03:14:42 +0700

    Update resetrootpassword.sh

--- mysql/resetrootpassword.sh
+++ mysql/resetrootpassword.sh
@@ -1,4 +1,52 @@
 #!/usr/bin/env  bash
+###############################################################################
+###                                                               	        ###
+###            LAZYSERVERADMIN / MYSQL / RESETROOTPASSWORD.SH	      	    ###
+###                           by mudy45@github                              ###
+###                                                                 	    ###
+### Reset MySQL / MariaDB root password and maintain /root/.my.cnf          ###
+###                                                                        	###
+###-------------------------------------------------------------------------###
+###                                                                        	###
+### Version requirement                                                   	###
+### Op System		: Ubuntu 16.04.03 Xenial LTS                            ###
+### RDBMS			: MariaDB 10.1                                          ###
+###                                                                        	###
+###-------------------------------------------------------------------------###
+###                                                                         ###
+### License	    : GNU General Public License version 3        	            ###
+### Copyright   : Mudy Situmorang (mudy45@gmail.com)                        ###
+### Tested OS	: Ubuntu 16.04.3 LTS Xenial                                 ###
+### Github      : https://github.com/mudy45/sysadminscripts    	            ###
+### Version     : 0.1 alpha --> WARNING: DO NOT USE !!!!                    ###
+### File        : /mysql/resetrootpassword.sh		                        ###
+### Release		: Not Available                                             ###
+### Update      : 20170920                                                  ###
+###                                                                         ###
+###############################################################################
+
+###############################################################################
+### help()                                                             	    ###
+### show usage info                                                    	    ###
+###-------------------------------------------------------------------------###
+###                                                                  	    ###
+help() {
+	[[ -n ${1} ]] && echo -e "** ${1}\n"
+  	cat <<EOU
+Reset MySQL / MariaDB root password without knowing old password 
+or using .my.cnf file.
+
+Usage: 	resetrootpassword.sh
+		resetrootpassword.sh newrootpassword
+		-h | --help     	This message.
+
+	EOU
+ 	exit
+}
+###                                                                         ###
+###############################################################################
+
+
 
 systemctl stop mysql
 mysqld_safe --skip-grant-tables --skip-networking &
