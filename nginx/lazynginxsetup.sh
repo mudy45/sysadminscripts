66606e907fb67ca63ba7163a5d9042d2022eb6c1
mudy45 <mudy45@gmail.com>
2017-09-20 01:21:51 +0700

    Create lazynginxsetup.sh

---
+++ nginx/lazynginxsetup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env  bash
+###############################################################################
+###                                                               	        ###
+###                   LAZYSERVERADMIN / LASYNGINXSETUP.SH 	         	    ###
+###                             by mudy45@github                            ###
+###                                                                 	    ###
+### Why bother compiling. They wouldn't know any way, only for installing  	###
+### wordpress. Lazy admin will use this script to prevent typing more than  ###
+### necessary.                                                            	###
+###                                                                      	###
+###-------------------------------------------------------------------------###
+###                                                                         ###
+### License	    : GNU General Public License version 3        	            ###
+### Copyright   : Mudy Situmorang (mudy45@gmail.com)                        ###
+### Tested OS	: Ubuntu 16.04.3 LTS Xenial                                 ###
+### Github      : https://github.com/mudy45/sysadminscripts    	            ###
+### Version     : 0.1 alpha --> WARNING: DO NOT USE !!!!                    ###
+### File        : /nginx/lazynginxsetup.sh                                  ###
+### Release		: Not Available                                             ###
+### Update      : 20170920                                                  ###
+###                                                                         ###
+###############################################################################
+
+apt-get update
+apt-get install nginx
+
+### EOF
