94804667288d455982336c160a263bbab908a6b6
mudy45 <mudy45@gmail.com>
2017-09-28 12:19:59 +0700

    Update lazynginxconfigure.sh

--- nginx/lazynginxconfigure.sh
+++ nginx/lazynginxconfigure.sh
@@ -1,1 +1,3 @@
 mkdir -p /var/cache/nginx && chown -cR nginx:root /var/cache/nginx
+
+ perl -00 -ple 's{$}{ \\\n\t\t\t--add-module=\$(MODULESDIR)/ngx_pagespeed} if m/common_configure_flags :=/' rules > nu
